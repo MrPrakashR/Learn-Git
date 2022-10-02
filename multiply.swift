97014301a933b422043dcbd2bfbd4cbfaf95ff79
Prakash <prakash.inovant@gmail.com>
2022-10-02 13:30:19 +0530

    added multiply.swift

---
+++ multiply.swift
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.swift
+//  CoreDataDemo1
+//
+//  Created by Mihir Solanki  on 20/05/22.
+//
+
+class Multiply {
+    func multTwoNumber(a:Int,b:Int) -> Int {
+        return a*b
+    }
+}
+
+print("Mul of two number ",Multiply().multTwoNumber(a: 1, b: 2))
+
+
