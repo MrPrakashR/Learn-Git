0773566c516cc82baafcdd33f3a8cf350a696d68
Prakash <prakash.inovant@gmail.com>
2022-10-02 12:07:22 +0530

    initial commit

---
+++ Sum.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+public class Sum {
+    func addTwoNumber(a:Int,b:Int) -> Int {
+        return a+b
+    }
+}
+
+print(Sum().addTwoNumber(a: 1, b: 2))
